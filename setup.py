1cf75588f9bdd4e9e2338a796720b856752d9cce
kzambello <kevin.zambello@pi.infn.it>
2025-02-28 17:00:18 +0100

    Use find_packages() in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='qcd_tools',
     version='0.1.0',
-    packages=['qcd_tools'],
+    packages=find_packages(),
     install_requires=['numpy','scikit-learn','scipy','sympy'],
 )
